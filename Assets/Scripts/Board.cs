af5bd3046293546e5c237b16122d05210abd18b3
Çağlayan Deniz Doğanay <caglayandenizd@gmail.com>
2026-09-22 19:55:27 +0300

    board created in scene view
    Tiles are now colored and visible in the board.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -6,22 +6,16 @@ public class Board : MonoBehaviour
     public int height = 8;
     TileColors[,] grid;
 
+    public GameObject tilePrefab;
+    public
+
     void Start()
     {
-        grid = new TileColors[width, height];
 
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                grid[x, y] = (TileColors)Random.Range(0, 5);
-                //=> Enum'i type casting ile int e cevirdik.
-                //int sayi = (int)TileColor.Green; ==> 2
-            }
-        }
-        //8x8 board da grid in belirledigimiz renklerle random doldurulmasi.
-
+        CreateBoard();
         PrintBoard();
+        CreateTiles();
+
 
     }
 
@@ -35,7 +29,7 @@ public class Board : MonoBehaviour
             {
                 text += grid[x, y].ToString()[0] + " ";
                 //burada da grid in console da gozukebilmesi icin string e cevirme islemi yapildi ve [0] ile ilk harfi alindi.            }
-                
+
             }
             //y yukardan basliyor cunku board umuzun y= 0 noktasi asagilarda olmasi lazim.
             text += "\n";
@@ -43,7 +37,93 @@ public class Board : MonoBehaviour
 
         }
         Debug.Log(text);
+    }
 
+
+    bool MakesMatch(int x, int y, TileColors color)
+    {
+        if (x >= 2)
+        {
+            TileColors left1 = grid[x - 1, y];
+            TileColors left2 = grid[x - 2, y];
+
+            if (left1 == color && left2 == color)
+            {
+                return true;
+            }
+        }
+
+        if (y >= 2)
+        {
+            TileColors down1 = grid[x, y - 1];
+            TileColors down2 = grid[x, y - 2];
+
+            if (down1 == color && down2 == color)
+            {
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
+    void CreateTiles()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Vector2 position = new Vector2(x, y);
+                GameObject tile = Instantiate(tilePrefab, position, Quaternion.identity);
+                Color color = GetColor(grid[x, y]);
+                tile.GetComponent<SpriteRenderer>().color = color;
+            }
+        }
+
+
+    }
+
+    Color GetColor(TileColors tileColor)
+    {
+        if (tileColor == TileColors.Red) return Color.red;
+        if (tileColor == TileColors.Blue) return Color.blue;
+        if (tileColor == TileColors.Green) return Color.green;
+        if (tileColor == TileColors.Yellow) return Color.yellow;
+        if (tileColor == TileColors.Purple) return Color.purple;
+
+        return Color.white;
+
+    }
+
+    void CreateBoard()
+    {
+        grid = new TileColors[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                TileColors color;
+                //hucreler icerisinde renkleri kontrol etmemiz gerektigi icin en ic
+                //dongude tanimladik ve bu sekilde yazdirdik.
+                do
+                {
+                    color = (TileColors)Random.Range(0, 5);
+                    //=> Enum'i type casting ile int e cevirdik.
+                    //int sayi = (int)TileColor.Green; ==> 2
+
+                }
+                while (MakesMatch(x, y, color));
+
+                grid[x, y] = color;
+
+
+
+
+            }
+        }
+         //8x8 board da grid in belirledigimiz renk degisenlleriyle random doldurulmasi.
 
     }
 }
